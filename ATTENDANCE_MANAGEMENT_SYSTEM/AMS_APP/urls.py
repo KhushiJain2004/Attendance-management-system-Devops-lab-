bd75e6fb183102e465432f9a10a753ae7bed3d93
Palakgupta26 <142999560+Palakgupta26@users.noreply.github.com>
2023-11-03 22:38:14 +0530

    Added few lines

--- ATTENDANCE_MANAGEMENT_SYSTEM/AMS_APP/urls.py
+++ ATTENDANCE_MANAGEMENT_SYSTEM/AMS_APP/urls.py
@@ -1,8 +1,11 @@
-#URLs of App
 
 from django.contrib import admin
 from django.urls import path
 from AMS_APP import views
 
 urlpatterns = [
+    path('AdminLogin', views.AdminLogin, name='Admin_Login'),
+    path('Studentlogin', views.Studentlogin, name='Student_Login'),
+    path('TeacherLogin', views.TeacherLogin, name='Teacher_Login'),
 ]
+
--- ATTENDANCE_MANAGEMENT_SYSTEM/AMS_APP/views.py
+++ ATTENDANCE_MANAGEMENT_SYSTEM/AMS_APP/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 
-# Create your views here.
+def AdminLogin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('dashboard')
+        return render(request, 'AdminLogin.html', {'error_message': 'Invalid credentials'})
+
+    return render(request, 'AdminLogin.html')
+
+def Studentlogin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('student_dashboard')
+        return render(request, 'Studentlogin.html', {'error_message': 'Invalid credentials'})
+
+    return render(request, 'Studentlogin.html')
+
+def TeacherLogin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('teacher_dashboard')
+        return render(request, 'TeacherLogin.html', {'error_message': 'Invalid credentials'})
+
+    return render(request, 'TeacherLogin.html')
