dbbdab512bb2974496ddc8859fb1976f7eb34981
Palakgupta26 <142999560+Palakgupta26@users.noreply.github.com>
2023-11-05 21:00:53 +0530

    Updated views.py and url.py

--- ATTENDANCE_MANAGEMENT_SYSTEM/AMS_APP/urls.py
+++ ATTENDANCE_MANAGEMENT_SYSTEM/AMS_APP/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('AdminLogin', views.AdminLogin, name='Admin_Login'),
     path('Studentlogin', views.Studentlogin, name='Student_Login'),
     path('TeacherLogin', views.TeacherLogin, name='Teacher_Login'),
+    path('login', views.loginuser, name='login'),
 ]
 
--- ATTENDANCE_MANAGEMENT_SYSTEM/AMS_APP/views.py
+++ ATTENDANCE_MANAGEMENT_SYSTEM/AMS_APP/views.py
@@ -39,3 +39,20 @@ def TeacherLogin(request):
         return render(request, 'TeacherLogin.html', {'error_message': 'Invalid credentials'})
 
     return render(request, 'TeacherLogin.html')
+
+def loginuser(request):
+    if not request.user.is_anonymous:
+            return redirect('/')
+    if request.method=="POST":
+        username=request.POST.get('ID')
+        password=request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        
+        if user is not None:
+            login(request,user)
+            return redirect('/admindashboard')              #change dashboard according to where it is coming from
+        else:
+            messages.error(request, 'Wrong username or password')
+            return render(request,'login.html')               #change login.html
+    return render(request,'login.html')                       #change login.html
+    
