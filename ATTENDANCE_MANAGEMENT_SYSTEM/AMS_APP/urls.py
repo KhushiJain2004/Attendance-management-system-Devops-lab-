91a30b71a3b2055ece0b899db86ab7b35b1a6daa
Palakgupta26 <142999560+Palakgupta26@users.noreply.github.com>
2023-11-06 15:15:45 +0530

    Added urls.py and views.py

--- ATTENDANCE_MANAGEMENT_SYSTEM/AMS_APP/urls.py
+++ ATTENDANCE_MANAGEMENT_SYSTEM/AMS_APP/urls.py
@@ -8,5 +8,16 @@ urlpatterns = [
     path('Studentlogin', views.Studentlogin, name='Student_Login'),
     path('TeacherLogin', views.TeacherLogin, name='Teacher_Login'),
     path('login', views.loginuser, name='login'),
+    path('addnewstudent', views.addnewstudent, name='addnewstudent'),
+    path('AdminDashboard', views.AdminDashboard, name='AdminDashboard'),
+    path('class_mgmt', views.class_mgmt, name='class_mgmt'),
+    path('courseadd', views.courseadd, name='courseadd'),
+    path('index', views.index, name='index'),
+    path('student_mgmt', views.student_mgmt, name='student_mgmt'),
+    path('teacher_mgmt', views.teacher_mgmt, name='teacher_mgmt'),
+    path('teacher', views.teacher, name='teacher'),
+    path('viewclass', views.viewclass, name='viewclass'),
+    path('viewfaculty', views.viewfaculty, name='viewfaculty'),
+    path('viewstudent', views.viewstudent, name='viewstudent'),
 ]
 
--- ATTENDANCE_MANAGEMENT_SYSTEM/AMS_APP/views.py
+++ ATTENDANCE_MANAGEMENT_SYSTEM/AMS_APP/views.py
@@ -56,3 +56,76 @@ def loginuser(request):
             return render(request,'login.html')               #change login.html
     return render(request,'login.html')                       #change login.html
     
+def addnewstudent(request):
+    if request.method == 'POST':
+        student_name = request.POST.get('student')
+        student_id = request.POST.get('studentName')
+        password = request.POST.get('password')
+
+        return HttpResponse('Student added successfully')
+    else:
+        return render(request, 'addnewstudent.html')
+
+
+def AdminDashboard(request):
+    context = {
+        # Add any context data you want to pass to the template here
+    }
+
+    return render(request, 'AdminDashboard.html', context)
+
+def class_mgmt(request):
+    context = {
+        # You can add any context data you want to pass to the template here
+    }
+    return render(request, 'class_mgmt.html', context)
+
+def courseadd(request):
+    context = {
+        # You can add any context data you want to pass to the template here
+    }
+    return render(request, 'courseadd.html', context)
+
+def index(request):
+    context = {
+        # You can add any context data you want to pass to the template here
+    }
+    return render(request, 'index.html', context)
+
+def student_mgmt(request):
+    context = {
+       
+    }
+    return render(request, 'student_mgmt.html', context)
+
+def teacher_mgmt(request):
+    context = {
+       
+    }
+    return render(request, 'teacher_mgmt.html', context)
+
+def teacher(request):
+    context = {
+        # You can add any context data you want to pass to the template here
+    }
+    return render(request, 'teacher.html', context)
+
+def viewclass(request):
+    context = {
+        # You can add any context data you want to pass to the template here
+    }
+    return render(request, 'viewclass.html', context)
+
+def viewfaculty(request):
+    context = {
+        # You can add any context data you want to pass to the template here
+    }
+    return render(request, 'viewfaculty.html', context)
+
+def viewstudent(request):
+    context = {
+        # You can add any context data you want to pass to the template here
+    }
+    return render(request, 'viewstudent.html', context)
+
+
